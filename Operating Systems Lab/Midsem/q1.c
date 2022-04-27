38c53ba27750da71327260ce8781f94433a40b94
Aditya Chandra <74142348+adityachandra1@users.noreply.github.com>
2022-04-27 09:11:50 +0530

    Update q1.c

--- Operating Systems Lab/Midsem/q1.c
+++ Operating Systems Lab/Midsem/q1.c
@@ -8,14 +8,14 @@ int main(int argc, char **argv){
 	pid_t fork_return = fork();
 	if(fork_return == 0){
 		printf("child\n");
-		printf("Size: %ld\n", (sizeof(argv[0])/sizeof(char)));
+		printf("Size: %ld\n", (sizeof(argv[1])/sizeof(char)));
 	} else if (fork_return > 0){
 		printf("Parent\n");
 		int count = 0 ;
-		long int n = (sizeof(argv[0])/sizeof(char));
+		long int n = (sizeof(argv[1])/sizeof(char));
 		for(int i = 0; i < n; i++){
-			if(argv[0][i] == 'a' || argv[0][i] == 'e' ||argv[0][i] == 'i' ||argv[0][i] == 'o' ||argv[0][i] == 'u'){
-				printf("%c\n", argv[0][i]);
+			if(argv[1][i] == 'a' || argv[1][i] == 'e' ||argv[1][i] == 'i' ||argv[1][i] == 'o' ||argv[1][i] == 'u'){
+				printf("%c\n", argv[1][i]);
 				count++;
 			}
 		}
