3e9c61e66611d44fc437cd5c70ab2e6f32f1f613
Aditya Chandra <74142348+adityachandra1@users.noreply.github.com>
2022-05-10 16:47:12 +0530

    lab-7

---
+++ Algorithms Lab/Lab-7/A1_Merge_K_Sorted.cpp
@@ -0,0 +1,42 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+void merge_and_sort(
+	vector<int> &output, vector<vector<int>> arr,
+	int n, int k)
+{
+	// Put the elements in sorted array.
+	for (int i = 0; i < k; i++) {
+		for (int j = 0; j < n; j++) {
+			output[(i * n) + j] = arr[i][j];
+		}
+	}
+	sort(output.begin(), output.end());
+}
+int main()
+{
+	vector<vector<int>> arr;
+    int k;
+    cout << "enter no of arrays (k): " << endl;
+    cin >> k;
+	cout << "enter size of arrays(n): " << endl;
+	int n;
+	cin >> n;
+
+	for(int i = 0 ; i < k ; i++){
+        vector<int> temp(n);
+        cout << "Enter elements in array " << i+1 << endl;
+        for(int j = 0 ; j < n ; j++){
+            cin >> temp[j];
+        }
+        arr.push_back(temp);
+	}
+
+	vector<int> output(n*k);
+    cout << "Merged Arrays" << endl;
+	merge_and_sort(output, arr, n, k);
+	for (int i = 0; i < n * k; i++)
+		cout << output[i] << " ";
+
+	return 0;
+}
